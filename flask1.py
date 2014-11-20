0563c976d391deb95e5b0376fcda5ad1e2c2adc9
user <user@user-N56VJ.(none)>
2014-11-20 09:55:51 +0200

    added counter

--- flask1.py
+++ flask1.py
@@ -9,12 +9,24 @@ app= Flask(__name__)
 @app.route('/')
 
 def index():
+    return "i am in Rwanda"
+
+@app.route('/<kigali>')
+
+def rda(kigali):
     return "i am in capital of Rwanda"
 
-@app.route('/<rda>')
+@app.route('/greet/<name>')
 
-def rda(rda):
-    return "i am in Rwanda"
+def greet(name):
+    return "<h1>Mwaramutse, %s how is home?<h1>" % name
+
+@app.route('/counter/<int:num>')
+
+def counter(num):
+	dict = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five'}
+	return 'Number %s' % dict.get( num,'unknown')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
